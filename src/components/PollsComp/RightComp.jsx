ea4df407384e39f55cf97e0d784dfa1513c4f8a6
2gedareactfrontend <146445144+2gedareactfrontend@users.noreply.github.com>
2024-02-27 07:19:57 -0800

    Authandpoll (#8)
    * Ui changes based on the new endpoints responses
    
    * Ui changes based on the new endpoints responses
    
    * endpoints integration
    
    ---------
    
    Co-authored-by: misspelz <pelumiiadetoye@gmail.com>

--- src/components/PollsComp/RightComp.jsx
+++ src/components/PollsComp/RightComp.jsx
@@ -4,14 +4,14 @@ import React from "react";
 import { Notifications } from "./Notification";
 import { useNavigate } from "react-router-dom";
 
-export const PollsNotification = ({ setNotify, showCreateModal }) => {
+export const PollsNotification = ({ setNotify, onShowCreateModal }) => {
   const nav = useNavigate();
   return (
     <div className="py-10 md:py-4 px-6">
       {/* CREATE POLL ACTION */}
       <div
         className="flex items-center gap-6 mt-4 border px-4 py-2 rounded-[10px] cursor-pointer"
-        onClick={showCreateModal}
+        onClick={onShowCreateModal}
       >
         <img src="images/create.png" alt="create-icon" width={25} />
         <p className="text-[13px] font-[500] mt-3">Create poll</p>
